5df519a0a4b62087a91cf5161fed80b0c6255c5a
Zac Solomon <zac@zacsolo.com>
2020-08-29 18:00:57 -0400

    Create User Entity

--- LIREDDIT-SERVER/src/resolvers/post.ts
+++ LIREDDIT-SERVER/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from 'src/types';
 
